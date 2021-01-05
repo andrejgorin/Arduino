a1e087571bc3cc0d3ec6aafc40de8a7d8e4c01e7
agorins <agorins@evolutiongaming.com>
2021-01-05 15:06:49 +0200

    a

---
+++ MyThermostat2/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <TimeLib.h>
+#include <WidgetRTC.h>
+#include <Bounce2.h>
+
+/* where my sensitive info stored - MAIL, AUTH, SSID, PASS */
+#include "MyCredentials.h"
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
