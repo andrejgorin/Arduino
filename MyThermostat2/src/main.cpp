b7277e834059fdf38f77f5b026c0d6102caee080
agorins <agorins@evolutiongaming.com>
2021-01-05 15:12:47 +0200

    a

--- MyThermostat2/src/main.cpp
+++ MyThermostat2/src/main.cpp
@@ -8,10 +8,35 @@
 /* where my sensitive info stored - MAIL, AUTH, SSID, PASS */
 #include "MyCredentials.h"
 
-void setup() {
-  // put your setup code here, to run once:
+/* section to add your sensitive settings */
+const char *MYMAIL = MAIL;
+const char *MYAUTH = AUTH;
+const char *MYSSID = SSID;
+const char *MYPASS = PASS;
+
+struct element
+{
+  bool state;
+  int vbutton;
+  int rbutton;
+  int vled;
+  int rled;
+};
+
+/* perform actions after connected to blynk server */
+BLYNK_CONNECTED()
+{
+  Blynk.syncAll();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  /* setup connection to blynk server */
+  Blynk.begin(MYAUTH, MYSSID, MYPASS);
+}
+
+void loop()
+{
+  /* begin Blynk */
+  Blynk.run();
 }
