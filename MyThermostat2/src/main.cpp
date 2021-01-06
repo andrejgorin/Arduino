f6602768e1c4269b55cb93ff4856cdc6bc3cd621
agorins <agorins@evolutiongaming.com>
2021-01-06 14:29:38 +0200

    a

--- MyThermostat2/src/main.cpp
+++ MyThermostat2/src/main.cpp
@@ -1,3 +1,14 @@
+/* Testing some hardware and Blynk software */
+/* TODO: temperature sensor ds18b20 */
+/* TODO: i2c lcd screen */
+/* TODO: RTC DS3231 */
+/* TODO: ttl level converter TXS0108 */
+
+/* Comment this out to disable prints and save space */
+// #define BLYNK_PRINT Serial
+// #define BLYNK_DEBUG
+
+/* including public libraries */
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
@@ -14,7 +25,13 @@ const char *MYAUTH = AUTH;
 const char *MYSSID = SSID;
 const char *MYPASS = PASS;
 
-struct element
+/* declaration of functions */
+
+/* check if WiFi is connected */
+bool checkWiFi();
+
+/* example of some struct element */
+struct element // TODO not used now
 {
   bool state;
   int v_trigger;
@@ -22,6 +39,14 @@ struct element
   int v_executor;
   int r_executor;
 };
+
+/* blue led on board. should be on when wifi connected */
+struct blueLedOnBoard
+{
+  const int r_executor = 2;
+};
+
+blueLedOnBoard bLOB;
 
 /* perform actions after connected to blynk server */
 BLYNK_CONNECTED()
@@ -31,6 +56,10 @@ BLYNK_CONNECTED()
 
 void setup()
 {
+  /* set modes and initial states for pins */
+  pinMode(bLOB.r_executor, OUTPUT);
+  digitalWrite(bLOB.r_executor, HIGH);
+
   /* setup connection to blynk server */
   Blynk.begin(MYAUTH, MYSSID, MYPASS);
 }
@@ -39,4 +68,11 @@ void loop()
 {
   /* begin Blynk */
   Blynk.run();
+  digitalWrite(bLOB.r_executor, checkWiFi());
+}
+
+/* check if WiFi is connected */
+bool checkWiFi()
+{
+  return !Blynk.connected();
 }
