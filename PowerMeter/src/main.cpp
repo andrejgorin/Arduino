c7b73358bba10050e33cb9888b433116a1d504af
Andrejs Gorins <andrejgorin@gmail.com>
2022-01-21 21:23:51 +0200

    Power Meter

--- PowerMeter/src/main.cpp
+++ PowerMeter/src/main.cpp
@@ -2,15 +2,90 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include <WiFi.h>
+#include <PZEM004Tv30.h>
 #include "MyCredentials.h"
 
 #define IOT_PUBLISH_TOPIC "esp32/pub"
 #define IOT_SUBSCRIBE_TOPIC "esp32/sub"
 
+#define PZEM_RX_PIN 16
+#define PZEM_TX_PIN 17
+#define PZEM_SERIAL Serial2
+
+PZEM004Tv30 pzem(PZEM_SERIAL, PZEM_RX_PIN, PZEM_TX_PIN);
+
 WiFiClient net;
 PubSubClient client(net);
 
+float voltage = 0;
+float current = 0;
+float power = 0;
+float energy = 0;
+float frequency = 0;
+float pf = 0;
+
 void messageHandler(char *topic, byte *payload, unsigned int length);
+
+void myPzem()
+{ 
+  voltage = pzem.voltage();
+  current = pzem.current();
+  power = pzem.power();
+  energy = pzem.energy();
+  frequency = pzem.frequency();
+  pf = pzem.pf();
+
+  // Check if the data is valid
+  if (isnan(voltage))
+  {
+    Serial.println("Error reading voltage");
+  }
+  else if (isnan(current))
+  {
+    Serial.println("Error reading current");
+  }
+  else if (isnan(power))
+  {
+    Serial.println("Error reading power");
+  }
+  else if (isnan(energy))
+  {
+    Serial.println("Error reading energy");
+  }
+  else if (isnan(frequency))
+  {
+    Serial.println("Error reading frequency");
+  }
+  else if (isnan(pf))
+  {
+    Serial.println("Error reading power factor");
+  }
+  else
+  {
+
+    // Print the values to the Serial console
+    Serial.print("Voltage: ");
+    Serial.print(voltage);
+    Serial.println("V");
+    Serial.print("Current: ");
+    Serial.print(current);
+    Serial.println("A");
+    Serial.print("Power: ");
+    Serial.print(power);
+    Serial.println("W");
+    Serial.print("Energy: ");
+    Serial.print(energy);
+    Serial.println("kWh");
+    Serial.print("Frequency: ");
+    Serial.print(frequency);
+    Serial.println("Hz");
+    Serial.print("PF: ");
+    Serial.println(pf);
+  }
+
+  Serial.println();
+  delay(2000);
+}
 
 void connectWiFi()
 {
@@ -63,10 +138,12 @@ void publishMessage()
     connectBroker();
   }
   StaticJsonDocument<200> doc;
-  int hum = random(60, 70);
-  int temp = random(-20, -10);
-  doc["humidity"] = hum;    // CHANGEME
-  doc["temperature"] = temp; // CHANGEME
+  doc["voltage"] = voltage;
+  doc["current"] = current;
+  doc["power"] = power;
+  doc["energy"] = energy;
+  doc["frequency"] = frequency;
+  doc["pf"] = pf;
   doc["did"] = WiFi.macAddress();
   char jsonBuffer[512];
   serializeJson(doc, jsonBuffer); // print to client
@@ -96,4 +173,5 @@ void loop()
   publishMessage();
   client.loop();
   delay(1000);
+  myPzem();
 }
